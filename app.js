3858f2e287e0a8086ba6a264c7690c93aef3178a
Derek <derekdanielson84@gmail.com>
2023-07-24 09:20:51 -0600

    Added color change function.  JS complete

--- app.js
+++ app.js
@@ -4,5 +4,19 @@ const color = document.getElementById('color');
 const btn = document.getElementById('btn');
 
 // Generate random hex and apply color
-btn.addEventListener('click', changeColor());
+btn.addEventListener('click', changeColor);
 
+// Get random number
+function randomNumber(){
+    return Math.floor(Math.random() * hex.length);
+}
+
+// Change color function
+function changeColor(){
+    let hexColor = '#';
+    for (let i = 0; i < 6; i++){
+        hexColor += hex[randomNumber()];
+    }
+    color.textContent = hexColor;
+        document.body.style.backgroundColor = hexColor;
+}
