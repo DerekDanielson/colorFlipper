2a7e6e5c035be61419bce51cf449ec64b687aaaf
Derek <derekdanielson84@gmail.com>
2023-07-24 08:53:50 -0600

    Added event listener

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 const color = document.getElementById('color');
 const btn = document.getElementById('btn');
 
+// Generate random hex and apply color
+btn.addEventListener('click', changeColor());
+
